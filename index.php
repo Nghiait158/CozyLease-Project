28e0b078a860ff65567415e601e08721a57f56a0
Nghia <trinhvantrungnghia@gmail.com>
2024-03-04 01:09:21 +0700

    add  video background and fix size

--- index.php
+++ index.php
@@ -5,36 +5,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/all.min.css">
-    <title>CozyLease
-    </title>
+
+<!-- google font Madimi One-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Pacifico&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <title>CozyLease</title>
 </head>
 <body>
-    <!-- <h1>Home Page dmm</h1> -->
 <!-- Start Navigation -->
-<nav class="navbar navbar-expand-sm bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">CozyLease</a>
-    <span class="Navbar-text">Solutions for house rentals</span>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a href="" class="nav-link">Students</a></li>
-        <li class="nav-item"><a href="" class="nav-link">Universities</a></li>
-        <li class="nav-item"><a href="" class="nav-link">About Us</a></li>
-        <li class="nav-item"><a href="" class="nav-link">Login</a></li>
-        <li class="nav-item"><a href="" class="nav-link">Sign Up for free</a></li>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.php">CozyLease</a>
+          <span class="navbar-text">Solutions for house rentals</span>
+          <button class="navbar-toggler" type="button" 
+              data-toggle="collapse" 
+              data-target="#navbarNavAltMarkup" 
+              aria-controls="navbarNavAltMarkup" 
+              aria-expanded="false" 
+              aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <ul class="navbar-nav custom-nav pl-5">
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">House</a></li>
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Apartment</a></li>
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Business Solution</a></li>
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Manage Rentals</a></li>
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Log In</a></li>
+              <li class="nav-item custom-nav-item"><a href="#" class="nav-link">Sign Up</a></li>
 
 
-      </ul>
-    </div>
-  </div>
-</nav>
+            </ul>
+          </div>
+        </div>
+      </nav>
 <!-- End Navigation -->
 
-
-
+<!-- Start video background -->
+<div class="container-fluid remove-vid-marg">
+  <div class="vid-parent"></div>
+    <div>
+      <video playinline autoplay muted loop>
+        <source src="video/clipbackground.mp4" >
+      </video>
+    </div>
+</div>
+<!-- End video -->
 
 
 
