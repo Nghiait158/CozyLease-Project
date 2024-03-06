8b268267f7765a3c17485f318c09b723de68b91f
Nghia <trinhvantrungnghia@gmail.com>
2024-03-07 01:49:22 +0700

    add search box, add text front of video, create banner

--- index.php
+++ index.php
@@ -11,13 +11,20 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Madimi+One&family=Pacifico&display=swap" rel="stylesheet">
 
-    <!-- CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lora:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet"><!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=Lora:ital,wght@0,400..700;1,400..700&family=Pacifico&display=swap" rel="stylesheet">
+
     <title>CozyLease</title>
 </head>
 <body>
 <!-- Start Navigation -->
-      <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5">
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark pl-5 fixed-top">
         <div class="container-fluid">
           <a class="navbar-brand" href="index.php">CozyLease</a>
           <span class="navbar-text">Solutions for house rentals</span>
@@ -52,12 +59,37 @@
       <video playinline autoplay muted loop>
         <source src="video/clipbackground.mp4" >
       </video>
+      <div class="vid-overlay"></div>
+    </div>
+    <div class="vid-content">
+      <h1 class="my-content"> Welcome to CozyLease </h1>
+      <!-- <small class="my-content">Find your ideal place</small>
+      <a href= "#" class="btn btn-danger"> Start now</a> -->
+      <form class="d-flex" role="search">
+        <input class="form-control me-8" type="search" placeholder="Find your ideal place" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
 </div>
 <!-- End video -->
 
+<!-- Start banner -->
+<div class="container-fluid bg-danger text-banner">
+  <div class="row bottom-banner" >
+    <div class="col-sm">
+        <h2>
+          Properties for you
+          
+        </h2>
+        <h3>Find the ideal location, schedule a visit, or get in touch for additional information.</h3>
+        
+    </div>
+  </div>
+
+</div>
 
 
+<!-- End Banner -->
 
 
 
